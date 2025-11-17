9f56c58a522008d4f0a11a178e8a1899634aa799
VINOD-TEC <65216682+VINOD-TEC@users.noreply.github.com>
2025-11-17 13:11:13 +0530

    Update AWS region in Terraform backend configuration

--- jenkins-server-terraform/02_backend.tf
+++ jenkins-server-terraform/02_backend.tf
@@ -5,7 +5,7 @@ terraform {
     bucket         = "three-tier-ali-devsecops-bucket-s3"
     
     # The AWS region where the S3 bucket and DynamoDB table are located.
-    region         = "us-west-2"
+    region         = "us-east-1"
     
     # The key (path) within the S3 bucket for storing the state file.
     key            = "three-tier-devdecops-project/jenkins-server-terraform/terraform.tfstate"
@@ -31,3 +31,4 @@ terraform {
     }
   }
 }
+
