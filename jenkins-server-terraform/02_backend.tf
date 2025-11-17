32d8458010bcf6235a8e89a60484e5d810b2e553
VINOD-TEC <65216682+VINOD-TEC@users.noreply.github.com>
2025-11-17 13:35:07 +0530

    Update S3 bucket name for Terraform backend

--- jenkins-server-terraform/02_backend.tf
+++ jenkins-server-terraform/02_backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     # The S3 bucket where the Terraform state file will be stored.
-    bucket         = "three-tier-ali-devsecops-bucket-s3"
+    bucket         = "three-tier-ali-devsecops-state-west2"
     
     # The AWS region where the S3 bucket and DynamoDB table are located.
     region         = "us-west-2"
@@ -31,5 +31,6 @@ terraform {
     }
   }
 }
+
 
 
